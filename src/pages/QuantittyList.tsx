cc25f0ebfe6ccc89723833ee6404f97f2d5d8725
jamshidbazarbaev5 <your-github-email@example.com>
2025-03-11 07:09:38 +0500

    added date

--- src/pages/QuantittyList.tsx
+++ src/pages/QuantittyList.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { DataTable } from '../helpers/DataTable'
 import { PageHeader } from '../helpers/PageHeader'
 import { Edit, Trash2 } from 'lucide-react'
-gt
+
 export function QuantityList() {
   const navigate = useNavigate()
   const [quantities, setQuantities] = useState([])
