3020f2b7066fc3daf6d4fa53f8f60ba4f0d88889
jamshidbazarbaev5 <your-github-email@example.com>
2025-03-15 23:47:01 +0500

    fixxx

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/karsu-admin-panel/',
@@ -16,7 +15,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "/src"),
     },
   },
   publicDir: 'public'
